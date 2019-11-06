302ac585ac891ded4d59f8290d432511649e8a05
Tulsi Prasad <tulsi.prasad50@gmail.com>
2019-11-06 22:02:04 +0530

    refactor code working

--- app.js
+++ app.js
@@ -22,28 +22,9 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 
-// creating three dafualt posts:
-const item1 = new Post({
-  title: "home",
-  content: "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing."
-});
-
-const item2 = new Post({
-  title: "about",
-  content: "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing."
-});
-
-const item3 = new Post({
-  title: "contact",
-  content: "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing."
-});
-
-const itemSchema = {
-  title: String,
-  content: String
-};
-
-const Item = mongoose.model("Item", itemSchema);
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 // first step: to create a preferrable schema
 const postSchema = {
@@ -56,94 +37,52 @@ const Post = mongoose.model("Post", postSchema);
 
 app.get("/", function (req, res) {
 
-      Item.find({
-        title: "home"
-      }, function (err, foundDefaultItems) {
-        if (foundItems === 0) {
-          Post.insertMany([post1], function (err) {
-            if (err) {
-              console.log(err);
-            } else {
-              console.log("Successfully saved home text into home");
-            }
-          });
+  Post.find({}, function (err, posts) {
+    res.render("home", {
+      homeText: homeStartingContent,
+      posts: posts
+    });
+  });
+});
 
-          Post.find({}, function (err, foundUserItems) {
-            if (foundUserItems != 0) {
-              res.render("home", {
-                homeText: foundDefaultItems.content,
-                posts: foundUserItems
-              });
-            } else {
-              res.render("home", {
-                homeText: foundUserItems.content,
-                posts: []
-              });
-            }
-          });
-        } else {
-          Post.find({}, function (err, foundUserItems) {
-            if (foundUserItems != 0) {
-              res.render("home", {
-                homeText: foundDefaultItems.content,
-                posts: foundUserItems
-              });
-            } else {
-              res.render("home", {
-                homeText: foundUserItems.content,
-                posts: []
-              });
-            }
-          });
-        }
-      });
+app.get("/compose", function (req, res) {
+  res.render("compose", {});
+});
 
-      app.get("/contact", function (req, res) {
-        res.render("contact", {
-          contactText: contactContent
-        });
-      });
+// to get what the user is typing in the compose box
+app.post("/compose", function (req, res) {
+  const post = new Post({
+    title: req.body.titleText,
+    content: req.body.bodyText
+  });
 
-      app.get("/about", function (req, res) {
-        res.render("about", {
-          aboutText: aboutContent
-        });
-      });
+  post.save(function (err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
+});
 
-      app.get("/compose", function (req, res) {
-        res.render("compose", {});
-      });
+app.get("/posts/:postId", function (req, res) {
 
-      // to get what the user is typing in the compose box
-      app.post("/compose", function (req, res) {
-            const post = new Post({
-              title: req.body.titleText,
-              content: req.body.bodyText
-            });
-            // posts.push(post);
-            post.save(function (err) {
-              if (!err) {
-                res.redirect("/");
-              }
-            });
-            app.get("/posts/:topic", function (req, res) {
-                  Post.find({}, function (err, foundItems) {
-                    if (err) {
-                      console.log(err);
-                    } else {
-                      foundItems.forEach(function (element) {
-                        let x = element.title;
-                        let y = req.params.topic;
-                        if (_.kebabCase(x) === _.kebabCase(y)) {
-                          res.render("post", {
-                            postHeading: element.title,
-                            postContent: element.content
-                          });
-                        }
-                      });
-                    }
-                  });
+  const requestedPostId = req.params.postId;
 
-                  app.listen(3000, function () {
-                    console.log("Server started on port 3000");
-                  });
+  Post.findOne({_id: requestedPostId}, function (err, post) {
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", {aboutText: aboutContent});
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", {contactText: contactContent});
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
